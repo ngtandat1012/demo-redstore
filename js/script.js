7c6082189c9934b33c9b065f308557e88e7aeb00
ngtandat1012 <ngtandat1012@gmail.com>
2022-02-24 09:58:29 +0700

    demo-redstore

---
+++ js/script.js
@@ -0,0 +1,48 @@
+window.addEventListener("load",function(){
+
+
+    AOS.init();
+  
+  });
+  
+
+
+var MenuItems = document.getElementById("MenuItems");
+    MenuItems.style.maxHeight ="0px"; 
+    function menutoggle(){
+        if(MenuItems.style.maxHeight =="0px")
+        {
+            MenuItems.style.maxHeight="200px";
+        }
+        else{
+            MenuItems.style.maxHeight="0px";
+        }
+    }
+
+
+     // --------------------toggle form---------------
+        
+     var LoginForm = document.getElementById("LoginForm");
+     var RegForm = document.getElementById("RegForm");
+     var Indicator = document.getElementById("Indicator");
+ 
+     function register(){
+ 
+             RegForm.style.transform = "translateX(0px)";
+             LoginForm.style.transform = "translateX(0px)";
+             Indicator.style.transform = "translateX(100px)";
+         }
+         function logic(){
+ 
+             RegForm.style.transform = "translateX(300px)";
+             LoginForm.style.transform = "translateX(300px)";
+             Indicator.style.transform = "translateX(0px)";
+         }
+
+// ----------------------for product galleryl-----------------------
+
+
+    
+        
+        
+  
